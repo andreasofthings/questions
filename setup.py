b7cd51ab93e2df9445f2288177ce0e1c5ebb4c7b
Andreas Neumeier <andreas@neumeier.org>
2015-11-02 12:38:42 +0100

    Category.

--- setup.py
+++ setup.py
@@ -37,5 +37,6 @@ setup(
     ],
     dependency_links=[
         'https://github.com/aneumeier/python-django-social/tarball/master#egg=latest',
+        'https://github.com/aneumeier/category/tarball/master#egg=latest',
     ]
 )
