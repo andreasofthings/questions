f40ebef681f164d9c0d87f7044931d57c90dbdf6
Andreas Neumeier <andreas@neumeier.org>
2015-12-14 10:46:19 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -31,12 +31,11 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     dependency_links=[
+        'git+https://github.com/aneumeier/category/tarball/#egg=master',
     ],
     install_requires=[
-        'git+https://github.com/aneumeier/python-django-social/#egg=master',
-        'git+https://github.com/aneumeier/category/tarball/#egg=master',
-        # 'category==0.9',
-        # 'python-django-social>=0.1',
+        'category==0.9',
+        'python-django-social>=0.1',
         'django>=1.8.0',
         'django-braces',
         'python-dateutil==2.3',
